bbee0c2afb9568c2f957d7261c055f5f68e6fd1b
candicecz <czech.candice@gmail.com>
2021-11-19 22:35:29 -0500

    [ADD] mobile styles

--- src/lib/components/ParallaxSlider.svelte
+++ src/lib/components/ParallaxSlider.svelte
@@ -1,56 +1,36 @@
 <script>
-  import { ChevronLeftIcon, ChevronRightIcon } from 'svelte-feather-icons';
-  import { debug } from 'svelte/internal';
+  import { debounce } from '$lib/utils/helpers';
 
-  let activeSlide;
-  let slide = 0;
-  let x = 0;
-  export let slides;
-  // export let newSlides;
+  export let slidesData;
   export let title;
   export let title2;
   export let titleFontClassName;
-  export let title2FontClassName;
+  export let title2FontClassName = '';
 
-  // console.log(newSlides)
+  // [isMobile] means the user is on a mobile device AND in landscape mode.
+  export let isMobile = false;
 
-  const nextSlide = () => {
-    // {console.log ("hello world from next slide")}
-    if (slide < slides.length - 1) {
-      activeSlide.style.transform = `translate3d(-${slide + 1}00vw, 0, 0)`;
-      slide += 1;
-    } else {
-      activeSlide.style.transform = `translate3d(0vw, 0, 0)`;
-      slide = 0;
-    }
-  };
+  // Inner width and height of window.
+  export let innerWidth;
+  export let innerHeight;
 
-  const prevSlide = () => {
-    if (slide === 0) {
-      activeSlide.style.transform = `translate3d(-200vw, 0, 0)`;
-      slide = slides.length - 1;
-    } else {
-      activeSlide.style.transform = `translate3d(-${slide - 1}00vw, 0, 0)`;
-      slide = slide - 1;
-    }
-  };
+  // [sliderEl] is the element that wraps all the project slides and translates based on which slide is active (e.g. slides[active_index]).
+  let sliderEl;
+
+  /*
+  In mobile, the text slides are condensed into one. So the [slides] array is shortened to only have the first text slide.
+  In the html, we iterate over the leftover text slides slidesData.slice(text_slides_index, slidesData.length) and input them to the main text slide.
+   */
+  $: slides = slidesData;
+  $: text_slide_index = slidesData.findIndex((s) => s.type === 'text');
+  $: slides = isMobile && slidesData ? slidesData.slice(0, text_slide_index + 1) : slidesData;
+
+  $: slide_els = []; // all slide elements
+  $: active_index = 0; // index of currently displayed slide.
 
   const Cursors = {
     RIGHT: 'right-cursor',
     LEFT: 'left-cursor'
-  };
-
-  const handleSliderClick = () => {
-    if (sliderCursor === Cursors.RIGHT) {
-      nextSlide();
-    } else if (sliderCursor === Cursors.LEFT) {
-      prevSlide();
-    }
-  };
-
-  const onActiveSlide = (n) => {
-    slide = n;
-    activeSlide.style.transform = `translate3d(-${n}00vw, 0, 0)`;
   };
 
   let sliderCursor = 'cursor';
@@ -60,9 +40,90 @@
     const cursorXPosition = event.clientX;
     sliderCursor = wrapperWidth / 2 <= cursorXPosition ? Cursors.RIGHT : Cursors.LEFT;
   }
+
+  /*
+    - SLIDE CHANGE  ------------------------
+    Assuming that the desktop slides = 100vw and mobile slides have a 5:8 aspect ratio
+    */
+  let aspectRatio = { mobile: 8 / 5 };
+  $: mobile_width = Math.round(innerHeight * aspectRatio.mobile); //in px
+  $: DESKTOP_WIDTH = 100; //in vw
+
+  // Calculates the offset for the x translation.
+  let calculateXOffset = (w) => {
+    // [offset] is calculated by multiplying the width by the active index.
+    let offset = active_index * w;
+    // When on mobile, the last slide has the image from the previous slide showing on the left.
+    if (isMobile && active_index === slides.length - 1) {
+      // calculate the left padding by taking the width of the window - the current slide width
+      let left_padding = innerWidth - mobile_width;
+      offset = offset - left_padding;
+    }
+    return 0 - offset;
+  };
+
+  // Handles the translation on [sliderEl] to move to the next slide.
+  const nextSlide = () => {
+    // On last slide of project, go to next project index.
+    if (isMobile && active_index === slides.length - 1) {
+      return updateProjectIndex(id + 1);
+    }
+    // Update active index.
+    active_index = active_index < slides.length - 1 ? active_index + 1 : 0;
+    let translate_x = isMobile
+      ? `${calculateXOffset(mobile_width)}px`
+      : `${calculateXOffset(DESKTOP_WIDTH)}vw`;
+
+    return (sliderEl.style.transform = `translateX(${translate_x})`);
+  };
+
+  // Handles the translation on [sliderEl] to move to the previous slide.
+  const prevSlide = () => {
+    // On last slide of project, go to previous project index.
+    if (isMobile && active_index === 0) {
+      return updateProjectIndex(id - 1);
+    }
+    // Update active index.
+    active_index = active_index === 0 ? slides.length - 1 : active_index - 1;
+    let translate_x = isMobile
+      ? `${calculateXOffset(mobile_width)}px`
+      : `${calculateXOffset(DESKTOP_WIDTH)}vw`;
+
+    return (sliderEl.style.transform = `translateX(${translate_x})`);
+  };
+
+  // Handles the users click depending on which side of the page it's on.
+  const handleSliderClick = () => {
+    if (sliderCursor === Cursors.RIGHT) {
+      nextSlide();
+    } else if (sliderCursor === Cursors.LEFT) {
+      prevSlide();
+    }
+  };
+
+  // -------------------------
+
+  // - MOBILE SCROLL -----------------------
+
+  export let updateProjectIndex;
+  export let id;
+
+  const handleScrollDown = debounce((e) => {
+    const { scrollHeight, scrollTop } = e.target;
+    // Detect if user is at the bottom of the window to move to next slide.
+    if (scrollTop + innerHeight >= scrollHeight - 100) {
+      nextSlide();
+    }
+  }, 100);
+  // -------------------------
 </script>
 
-<div class="slider-wrapper" bind:clientWidth={wrapperWidth} on:mousemove={handleMousemove}>
+<div
+  class="slider-wrapper"
+  bind:clientWidth={wrapperWidth}
+  on:mousemove={handleMousemove}
+  style={isMobile ? `width:${innerWidth}px; height:${innerHeight}px` : ''}
+>
   <img class="image-logo" src="images/LOGO-Ai small_Super Bonjour smaller.svg" alt="Logo" />
   <img class="image-logo mobile" src="images/LOGOFACE-Ai.svg" alt="Logo" />
   <h2 class="slider-title">
@@ -71,59 +132,89 @@
   </h2>
   <div
     class={`slider ${sliderCursor}`}
-    bind:this={activeSlide}
+    bind:this={sliderEl}
     on:click={handleSliderClick}
-    style={`width: ${slides.length}00vw`}
+    style={`width: ${isMobile ? `${mobile_width * slides.length}px` : `${slides.length * 100}vw`}`}
   >
     {#each slides as slide, i}
-      {#if slide.type === 'image'}
-        <div
-          id={i}
-          class={`slide ${sliderCursor}`}
-          style={`background-position: ${i}00vw center; background-image: url(${slide.src})`}
-        />
-      {:else if slide.type === 'video'}
-        <div
-          id={i}
-          class={`slide slide-video ${sliderCursor} ${
-            slide.addPadding ? 'slide-video-extra-padding' : ''
-          }`}
-          style={`background-position: ${i}00vw center; position: relative;`}
-        >
-          <div class="video-container">
-            <!-- svelte-ignore a11y-media-has-caption -->
-            <video src={slide.src} autoplay="true" loop muted playsinline />
+      <div
+        class="slider-slide"
+        type={slide.type}
+        bind:this={slide_els[i]}
+        style={`width:${isMobile ? `${Math.round(innerHeight * aspectRatio.mobile)}px` : '100vw'};`}
+      >
+        {#if slide.type === 'image'}
+          <div
+            id={i}
+            class={`slide ${sliderCursor}`}
+            style={`background-position: ${i}00vw center; background-image: url(${slide.src})`}
+          />
+        {:else if slide.type === 'video'}
+          <div
+            id={i}
+            class={`
+            slide
+            slide-video
+            ${sliderCursor}
+            ${slide.addPadding ? 'slide-video-extra-padding' : ''}`}
+            style={`
+            background-position: ${i}00vw center;
+            position: relative;
+            ${isMobile ? '' : 'width:100vw'}
+            `}
+          >
+            <div class="video-container">
+              <!-- svelte-ignore a11y-media-has-caption -->
+              <video src={slide.src} autoplay="true" loop muted playsinline />
+            </div>
           </div>
-        </div>
-      {:else if slide.type === 'two-columns'}
-        <div class="slide two-columns-slide">
-          <div class="slide-column slide-left-column">
-            <video src={slide.videoSrc} autoplay="true" loop muted playsinline />
+        {:else if slide.type === 'two-columns'}
+          <div class="slide two-columns-slide">
+            <div class="slide-column slide-left-column">
+              <video src={slide.videoSrc} autoplay="true" loop muted playsinline />
+            </div>
+            <div class="slide-column slide-right-column">
+              <!-- svelte-ignore a11y-img-redundant-alt -->
+              <img src={slide.imageSrc} alt="left column image" />
+            </div>
           </div>
-          <div class="slide-column slide-right-column">
-            <!-- svelte-ignore a11y-img-redundant-alt -->
-            <img src={slide.imageSrc} alt="left column image" />
+        {:else}
+          <div
+            class="slide text_slide"
+            style={`
+            background-color: ${slide.backgroundColor}; color:${slide.color};
+            font-family: ${slide.font || 'moret'};
+            font-size: ${slide.fontSize};
+            overflow-y:${isMobile ? 'scroll' : 'auto'};
+            width:100%;
+            `}
+            on:scroll={handleScrollDown}
+          >
+            {#if isMobile}
+              <!-- All the text is encapsulated in one slide for mobile -->
+              {#each slidesData.slice(text_slide_index, slidesData.length) as textSlide}
+                <div style="padding:1rem 2rem">
+                  <h5 class="text_title">
+                    {textSlide.title}
+                  </h5>
+                  <p>{@html textSlide.src}</p>
+                </div>
+              {/each}
+            {:else}
+              <div class="text_slide_container">
+                <h5 class="text_title">
+                  {slide.title}
+                </h5>
+                {@html slide.src}
+              </div>
+            {/if}
           </div>
-        </div>
-      {:else}
-        <div
-          class="slide text_slide"
-          style={`background-color: ${slide.backgroundColor}; color:${slide.color}; font-family: ${
-            slide.font || 'moret'
-          }; font-size: ${slide.fontSize}`}
-        >
-          <div class="text_slide_container">
-            <h5 class="text_title">
-              {slide.title}
-            </h5>
-            {@html slide.src}
-          </div>
-        </div>
-      {/if}
+        {/if}
+      </div>
     {/each}
   </div>
   <div class="paginator">
-    <h4>{slide + 1} / {slides.length}</h4>
+    <h4>{active_index + 1} / {slides.length}</h4>
   </div>
 </div>
 
@@ -219,6 +310,11 @@
     line-height: 130%;
   }
 
+  .text_slide_container_mobile {
+    width: 100%;
+    padding: 2rem;
+  }
+
   .text_title {
     padding-top: 50px;
     padding-bottom: 10px;
@@ -245,12 +341,12 @@
     position: relative;
     display: flex;
     flex-wrap: nowrap;
-    transition: all 200ms ease-out 0s;
+    transition: all 200ms linear 0s;
+    background: #c3862c;
   }
 
   .slide {
     height: 100vh;
-    width: 100vw;
     background-size: cover;
     transition: all 200ms ease-out 0s;
   }
@@ -262,6 +358,7 @@
     width: 100vw;
     overflow: hidden;
     transition: 0.3s all;
+    background: #c3862c;
   }
 
   .arrow {
@@ -421,5 +518,8 @@
       top: 10px;
       left: 25px;
     }
+  }
+  @media screen and (orientation: landscape) and (max-height: 499px) {
+    /* For mobile-size but acts on desktop as well */
   }
 </style>
