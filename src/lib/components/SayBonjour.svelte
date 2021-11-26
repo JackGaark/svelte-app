f846fab41b6cb79d10432a0903df4aedc79b9dae
jack gaarkeuken <jack.gaarkeuken@gmail.com>
2021-11-26 14:36:02 -0500

    adujst font size modal

--- src/lib/components/SayBonjour.svelte
+++ src/lib/components/SayBonjour.svelte
@@ -88,6 +88,7 @@
   }
   .bonjour-dialog-container h1 {
     font-weight: 100;
+    font-size: 1.125rem;
     font-family: 'Opposit-Medium';
     color: #fff;
   }
@@ -103,6 +104,7 @@
   .bonjour-dialog-container .footer {
     color: #000;
     margin-top: 60px;
+    font-size: 0.875rem;
     font-family: 'roc-grotesk';
   }
 
